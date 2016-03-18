7ef13ae77a3367f0882e1563d68205ddd001374d
Tyler Finley <tylerfinley1@Tylers-MacBook-Pro.local>
2016-03-18 13:29:57 -0700

    add repo functionality

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,18 +1,38 @@
 var apiKey = require('./../.env').apiKey;
 
-
-
-$(document).ready(function(){
-  $('#showUser').click(function(){
+$(document).ready(function() {
+  $('#showUser').click(function(e) {
+    e.preventDefault();
     var username = $('#username').val();
+    var info   = 'https://api.github.com/users/'+ username;
+    var repoInfo  = 'https://api.github.com/users/' + username + '/repos';
     $('#username').val("");
-    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    $('.showUser').text("the repo is " + username + ".");
-    $('.showPicture').append('<img src=' + response.avatar_url + '>' );
-    $('.showRepo').text('https://api.github.com/users/' + username + '/repos')
-
-
+      $.getJSON(info + '?access_token=' + apiKey + '&per_page=90').then(function(response){
+        $('.profile').append("<img src=" + response.avatar_url + "/>");
+        $('.profile').append("<p>" +'UserName: ' + username + "</p>");
+      }).fail(function(error){
+        console.log(error.responseJSON.message);
+      });
+    $.getJSON(repoInfo + '?access_token=' + apiKey + '').then(function(repo){
+      for (var c = 0; c <= repo.length; c++) {
+        if (repo[c].name !== null || repo[c].name !== "") {
+          $('.profile').append(" <Br>Repository: " + repo[c].name);
+        } else {
+          $('.profile').append(" <Br>Repository: N/A");
+        }
+        if (repo[c].description !== null ||repo[c].description !== "") {
+          $('.profile').append(" <Br>Description: " + repo[c].description);
+        } else {
+          $('.profile').append(" <Br>Description: N/A");
+        }
+        if (repo[c].description !== null) {
+          $('.profile').append(" <Br>Languange: " + repo[c].language + "<hr>");
+        } else {
+          $('.profile').append(" <Br>Languange: N/a <hr>");
+        }
+      }
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
     });
   });
-
 });
